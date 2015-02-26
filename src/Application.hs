db0fe90334e1e61ece1511f8a1465e5cdb92f9af
Sid Raval <sid@thoughtbot.com>
2015-02-26 11:24:34 -0500

    Remove Heist, Session, and Auth default stuff

--- src/Application.hs
+++ src/Application.hs
@@ -14,18 +14,9 @@ import Snap.Snaplet.Session
 
 ------------------------------------------------------------------------------
 data App = App
-    { _heist :: Snaplet (Heist App)
-    , _sess :: Snaplet SessionManager
-    , _auth :: Snaplet (AuthManager App)
-    }
 
 makeLenses ''App
 
-instance HasHeist App where
-    heistLens = subSnaplet heist
-
-
 ------------------------------------------------------------------------------
 type AppHandler = Handler App App
-
 
--- src/Main.hs
+++ src/Main.hs
@@ -74,7 +74,7 @@ main = do
     -- directories, those are picked up automatically by the splice.
     (conf, site, cleanup) <- $(loadSnapTH [| getConf |]
                                           'getActions
-                                          ["snaplets/heist/templates"])
+                                          [])
 
     _ <- try $ httpServe conf site :: IO (Either SomeException ())
     cleanup
