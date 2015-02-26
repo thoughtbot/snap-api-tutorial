fd61daf55736f535aa099ea2b05cf486c97473d2
Sid Raval <sid@thoughtbot.com>
2015-02-26 11:52:37 -0500

    Add /status handler to Api snaplet

--- src/api/Core.hs
+++ src/api/Core.hs
@@ -5,11 +5,21 @@ module Api.Core where
 
 import Control.Lens
 import Snap.Snaplet
+import Snap.Core
+import qualified Data.ByteString.Char8 as B
 
 data Api = Api
 
 makeLenses ''Api
 
+apiRoutes :: [(B.ByteString, Handler b Api ())]
+apiRoutes = [("status", method GET respondOk)]
+
+respondOk :: Handler b Api ()
+respondOk = do
+  modifyResponse . setResponseCode $ 200
+
 apiInit :: SnapletInit b Api
 apiInit = makeSnaplet "api" "Core Api" Nothing $ do
+        addRoutes apiRoutes
         return Api
